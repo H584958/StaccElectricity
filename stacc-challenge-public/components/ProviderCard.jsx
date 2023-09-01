0e2ebf83ade02a5cab7db210ca062e5521545803
Simon Kobbenes <simon.kobbenes@gmail.com>
2023-09-01 17:36:49 +0200

    calculated and visualised  monthly consumption and price

--- stacc-challenge-public/components/ProviderCard.jsx
+++ stacc-challenge-public/components/ProviderCard.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './ProviderCard.module.css';
 
-export default function ProviderCard(providerItem) {
+export default function ProviderCard({ provider, monthlyConsumption }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMoreInfo = () => {
@@ -10,18 +10,16 @@ export default function ProviderCard(providerItem) {
   return (
     <div className={styles.card}>
       <div className={styles.cardHeader}>
-        <p>{providerItem.provider.name}</p>
-        <p>{providerItem.provider.price}/kWh</p>
-        <p>Total Cost</p>
+        <p>{provider.name}</p>
+        <p>{provider.price} kWh</p>
+        <p>{Math.round(monthlyConsumption(provider.month))} NOK</p>
       </div>
       <div className={styles.cardInformation}>
         <div className={styles.cardInfoBubbles}>
-          <div className={styles.cardInfoBubble}>
-            {providerItem.provider.pricingModel}
-          </div>
-          {providerItem.provider.pricePeriod && (
+          <div className={styles.cardInfoBubble}>{provider.pricingModel}</div>
+          {provider.pricePeriod && (
             <div className={styles.cardInfoBubble}>
-              Fixed price period: {providerItem.provider.pricePeriod} months
+              Fixed price period: {provider.pricePeriod} months
             </div>
           )}
         </div>
